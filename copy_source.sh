654348aff6ace544d80734628f51b05f220240fb
Josselin Auguste <jauguste@iblop.net>
2016-01-22 16:29:56 +0100

    Remove safari extension

--- copy_source.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cp source/bitbucket-issues-enlighter.js Bitbutcket\ issues\ enlighter.safariextension
-cp source/bitbucket-issues-enlighter.js google_chrome
-
-cp source/bitbucket-issues-enlighter.css Bitbutcket\ issues\ enlighter.safariextension
-cp source/bitbucket-issues-enlighter.css google_chrome
